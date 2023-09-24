983dc8e8872dde326f9cc1d2ed64fa04d6f767a3
Mahmoud Piraste <mahmoudpiraste@yahoo.com>
2023-09-24 14:57:07 +0330

    two

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ export default function Home() {
       <Head>
         <title>Bitex</title>
         <meta name="description" content="Best Assistant for your trading" />
+
+        
         <link rel="icon" href="/favicon.ico" />
         
       </Head>
